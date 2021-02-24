45f82e7389907f8abd3f0262e966728fc9224141
promehedi <mmhasan563@gmail.com>
2021-02-25 00:05:58 +0600

    Get Order By Id

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,6 +1,23 @@
 import asyncHandler from 'express-async-handler'
 import Order from '../models/orderModel.js'
 
+// @desc    Get Order By ID
+// @route   POST /api/v1/orders/:id
+// @access  Private
+export const getOrderById = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id).populate(
+    'user',
+    'name email address'
+  )
+
+  if (order) {
+    res.json(order)
+  } else {
+    res.status(404)
+    throw new Error('Order not found!')
+  }
+})
+
 // @desc    Create New Order
 // @route   POST /api/v1/orders
 // @access  Private
--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import { addNewOrder } from '../controllers/orderController.js'
+import { addNewOrder, getOrderById } from '../controllers/orderController.js'
 import { protect } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
 // Create New Order
 router.route('/').post(protect, addNewOrder)
+router.route('/:id').get(protect, getOrderById)
 
 export default router
