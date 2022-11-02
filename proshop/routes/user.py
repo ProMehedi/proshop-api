9bbdfd9941f5f9bea89cbe4fe3e00e1e441fbb6a
promehedi <admin@promehedi.com>
2022-11-03 02:31:30 +0600

    Create Login Route

--- proshop/routes/user.py
+++ proshop/routes/user.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 from ..models.user import User
 from ..configs.db import db
 from ..schemas.user import userSchema
@@ -13,7 +13,7 @@ def register_user():
         return {"success": False, "message": "No data provided"}, 400
 
     if not request.is_json:
-        return {"success": False, "message": "Invalid data, Only JSON data can be expected"}, 400
+        return {"success": False, "message": "Invalid data, Only JSON data can be pass"}, 400
 
     if not request.json.get('firstName'):
         return {"success": False, "message": "First Name is required"}, 400
@@ -50,3 +50,32 @@ def register_user():
         "message": "User registered successfully",
         "data": userSchema(created_user)
     }, 201
+
+
+@user.post('/login')
+def login_user():
+    if not request.data:
+        return {"success": False, "message": "No data provided"}, 400
+
+    if not request.is_json:
+        return {"success": False, "message": "Invalid data, Only JSON data can be pass"}, 400
+
+    if not request.json.get('email'):
+        return {"success": False, "message": "Email is required"}, 400
+
+    if not request.json.get('password'):
+        return {"success": False, "message": "Password is required"}, 400
+
+    user = db.users.find_one({'email': request.json.get('email')})
+
+    if not user:
+        return {"success": False, "message": "User not found"}, 404
+
+    if not check_password_hash(user['password'], request.json.get('password')):
+        return {"success": False, "message": "Invalid credentials"}, 400
+
+    return {
+        "success": True,
+        "message": "User logged in successfully",
+        "data": userSchema(user)
+    }, 200
