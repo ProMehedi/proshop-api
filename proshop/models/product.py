4b63fb389e7a03f192f0231c28d7ddc0d4f8400d
promehedi <admin@promehedi.com>
2022-11-06 19:57:00 +0600

    Product Model Validation

--- proshop/models/product.py
+++ proshop/models/product.py
@@ -1,4 +1,5 @@
-from pydantic import BaseModel, Field, constr, conint, confloat
+from pydantic import BaseModel, Field, constr
+from typing import List
 from enum import Enum
 import datetime as dt
 
@@ -17,24 +18,33 @@ class Status(str, Enum):
     publish = 'publish'
 
 
+class Review(BaseModel):
+    name: str
+    rating: float = Field(ge=1, le=5)
+    comment: str
+    reviewBy: str
+    reviewFor: str
+
+
 class Product(BaseModel):
-    name: constr(min_length=3, strip_whitespace=True, strict=True)
+    name: str = Field(min_length=3, strip_whitespace=True, strict=True)
     slug: constr(strip_whitespace=True, strict=True)
-    desc: str = None
-    short_desc: constr(min_length=10, strip_whitespace=True,
-                       strict=True, curtail_length=100)
-    price: confloat(ge=0.0)
-    regular_price: confloat(ge=0.0) = None
-    category: str = None
+    desc: str = Field(min_length=10, default="")
+    shortDesc: constr(min_length=10, strip_whitespace=True,
+                      strict=True, curtail_length=100) = ""
+    price: float = Field(ge=0.0, default=0.0)
+    regular_price: float = Field(ge=0.0, default=None)
+    category: str = ""
     type: Type = Type.simple
     status: Status = Status.publish
     featured: bool = False
-    quantity: conint(ge=0)
+    quantity: int = Field(ge=0, default=0)
     sku: constr(min_length=3, strip_whitespace=True, strict=True) = None
     images: list = Field(default=[])
     attributes: list = Field(default=[])
     categories: list = Field(default=[])
-    rating: confloat(ge=0.0, le=5.0) = 0.0
+    rating: float = Field(ge=0.0, le=5.0, default=None)
     created_at: str = Field(default=dt.datetime.utcnow())
-    updated_at: str = None
+    updated_at: str = ""
     user_id: str
+    reviews: List[Review] = []
--- test/index.py
+++ test/index.py
@@ -1,3 +1,5 @@
+import simplejson
+import json
 from pydantic import BaseModel, ValidationError, validator
 from email_validator import validate_email, EmailNotValidError
 import phonenumbers
@@ -56,14 +58,65 @@ from phonenumbers.phonenumberutil import number_type
 # valid = carrier._is_mobile(number_type(phonenumbers.parse(number)))
 # print(valid)
 
-user = {'_id': '6362cd8f5d97edcf89a23a70',
-        'first_name': 'Mehedi', 'last_name': 'Hasan',
-        'email': 'mehedi@example.com', 'phone': '+8801782927925',
-        'password': 'pbkdf2:sha256:260000$jtCKC8nQEDWX1o88$d22ff9d2b8f03e631d19704482e287a622aa1a6fe08559f0141bbb4c247d6ff9', 'role': 'admin', 'created_at': '2021-05-09 12:00:00'}
+# user = {'_id': '6362cd8f5d97edcf89a23a70',
+#         'first_name': 'Mehedi', 'last_name': 'Hasan',
+#         'email': 'mehedi@example.com', 'phone': '+8801782927925',
+#         'password': 'pbkdf2:sha256:260000$jtCKC8nQEDWX1o88$d22ff9d2b8f03e631d19704482e287a622aa1a6fe08559f0141bbb4c247d6ff9', 'role': 'admin', 'created_at': '2021-05-09 12:00:00'}
 
-# Remove password from user dict
-user.pop('password')
-print(user)
+# # Remove password from user dict
+# user.pop('password')
+# print(user)
 
 # new_user = dict(user).pop('password')
 # print(new_user)
+
+from pydantic import BaseModel, Field, constr, conint, confloat
+from typing import List
+import os
+import pymongo
+from bson.objectid import ObjectId
+import datetime as dt
+
+
+class Review(BaseModel):
+    name: str
+    rating: int = Field(ge=1, le=5)
+    comment: str
+
+
+class Product(BaseModel):
+    name: str
+    price: float = Field(ge=0)
+    reviews: List[Review] = []
+
+
+client = pymongo.MongoClient(
+    'mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.6.0')
+db = client['proshop']
+
+product = Product(name='test', price=100.00)
+review = Review(name='test', rating=5, comment='test')
+
+# created_product = db.products.insert_one(dict(product))
+# created_review = db.reviews.insert_one(dict(review))
+
+# db.products.find_one_and_update({'_id': created_product.inserted_id}, {
+#     '$push': {'reviews':  ObjectId(created_review.inserted_id)}})
+
+# db.reviews.find_one_and_update({'_id': created_review.inserted_id}, {
+#     '$set': {'product': ObjectId(created_product.inserted_id)}})
+
+# new_review = db.reviews.insert_one(dict(review))
+# db.products.find_one_and_update({'_id': ObjectId('6366dde205467979d841ab1c')}, {
+#     '$push': {'reviews': ObjectId(new_review.inserted_id)}})
+
+# Get the product with reviews
+product = db.products.find_one({'_id': ObjectId('6366dde205467979d841ab1c')})
+
+# Get the reviews
+if product:
+    reviews = db.reviews.find({'_id': {'$in': product['reviews']}})
+    product['reviews'] = list(reviews)
+
+result = json.loads(simplejson.dumps(product, ignore_nan=True, default=str))
+print(json.dumps(result, indent=2))
